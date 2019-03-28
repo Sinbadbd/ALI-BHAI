593988ab3ccf5629fd66db97c4cdba7f3f5064c7
sinbad <sinbad@sinbads-Mac.local>
2019-03-27 23:31:27 -0700

    Initial Commit

---
+++ Ali Bhai/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ali Bhai
+//
+//  Created by sinbad on 3/27/19.
+//  Copyright © 2019 sinbad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
