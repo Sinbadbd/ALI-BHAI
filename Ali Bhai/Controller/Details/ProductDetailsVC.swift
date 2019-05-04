d45f3e9d6070365c5875b9695f3cafd90b41ded9
sinbad <sinbad@sinbads-Mac.local>
2019-05-04 12:59:54 -0700

    f

---
+++ Ali Bhai/Controller/Details/ProductDetailsVC.swift
@@ -0,0 +1,20 @@
+//
+//  ProductDetailsVC.swift
+//  Ali Bhai
+//
+//  Created by sinbad on 5/4/19.
+//  Copyright © 2019 sinbad. All rights reserved.
+//
+
+import UIKit
+
+class ProductDetailsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+    
+
+}
+// Image Slider
